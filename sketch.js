151b10cf702d21f2f539a4263b0c9bd53a6a5f96
Double Black <davish@catlin.edu>
2022-03-01 09:25:28 -0800

    Add background buildings

--- sketch.js
+++ sketch.js
@@ -4,11 +4,23 @@ const VERSION = '0.1.0';
 let keys = {};
 
 let game = {
-  test: 1
+  inMenu: true,
+  randomSeed: 0,
+  playerX: 0,
+  playerY: 0,
+  floor: 0,
+  cameraX: 0,
+  cameraY: 0,
 };
+
+let nathanImage;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  nathanImage = loadImage("https://i.imgur.com/qNRBIvl.png");
+
+  localStorage.clear(); // remove
 
   if (localStorage.getItem(LOCAL_STORAGE_KEY) != undefined) {
     loadGame();
@@ -20,7 +32,7 @@ function setup() {
 
 
 function reset() {
-  game.test = 0;
+  game.inMenu = true;
 }
 
 function saveGame() {
@@ -53,11 +65,26 @@ function update() {
 
 }
 
+function drawCity(offX, offY) {
+  randomSeed(13);
+
+  const BUILDING_COUNT = 32;
+  const ONE_MINUS_BC = BUILDING_COUNT - 1;
+  for (let l = 0; l < BUILDING_COUNT; l++) {
+    let x = floor(pow(random(-1, 1), 3) * 120);
+    let w = floor(random(5, 8) * ((l / ONE_MINUS_BC) * 0.25 + 0.75));
+    let h = floor(random(10, 50) * ((l / ONE_MINUS_BC) * 0.75 + 0.25));
+
+    fill(25 + pow(l / ONE_MINUS_BC, 2) * 100);
+    stroke(0);
+    rect(width / 2 + x * 20 - w * 50 + offX * ((l / ONE_MINUS_BC) * 0.75 + 0.25), height - h * 25 + offY * ((l / ONE_MINUS_BC) * 0.75 + 0.25), w * 50, height * 40);
+    
+  }
+}
+
 function draw() {
   update();
-  background(250);
-  fill(0);
-  text('Nathan Simulator 2' + VERSION, 40, 40);
-  text(JSON.stringify(keys), 40, 80);
-  text(localStorage.getItem(LOCAL_STORAGE_KEY), 40, 120);
+  background(20);
+  
+  drawCity(- mouseX +  width / 2, -mouseY * 2 + height);
 }
